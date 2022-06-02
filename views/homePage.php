68ec86adb85a8c4e62f4c3ef4d037642d5318898
Sara <pehlivansara@hotmail.com>
2022-06-02 19:46:38 +0200

    second_change

--- views/homePage.php
+++ views/homePage.php
@@ -1,20 +1,23 @@
-<div class="tetepage">
-    <img url="https://capp.nicepage.com/cbe4ba00f262be20fd3a468471fd81e45db5e255/images/default-logo.png"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <a href="#" class="fa fa-facebook"></a>
-    <a href="#" class="fa fa-twitter"></a>
-    <a href="#" class="fa fa-instagram"></a>
-</div>
-<div class="presentation"></div>
-<div class="titre">
-<h2>30 Day Fitness Challenge</h2>
-<P>Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</P>
-</div>
-<div class="textphoto">
-    <p>Each workout is only 20 minutes, but while the time commitment is minimal, the results you'll see are anything but: You'll notice you can do more reps within each timeframe, which means your fitness level is increasing. You'll also likely see some muscle definition and increased energy levels, while giving yourself a solid exercise foundation as you lean into 2021.</p>
-    <img src="https://images03.nicepage.com/c461c07a441a5d220e8feb1a/ce0e4dba656f5375961f7576/s.jpg?version=" />
-    <p>Images from
-        <a href="https://freepik.com/photos/people">Freepik</a>
-    </p>
+
+<div class="contenair">
+    <div class="tetepage">
+        <img url="https://capp.nicepage.com/cbe4ba00f262be20fd3a468471fd81e45db5e255/images/default-logo.png"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="#" class="fa fa-instagram"></a>
+    </div>
+    <div class="presentation"></div>
+    <div class="titre">
+        <h2>30 Day Fitness Challenge</h2>
+        <P>Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</P>
+    </div>
+    <div class="textphoto">
+        <p>Each workout is only 20 minutes, but while the time commitment is minimal, the results you'll see are anything but: You'll notice you can do more reps within each timeframe, which means your fitness level is increasing. You'll also likely see some muscle definition and increased energy levels, while giving yourself a solid exercise foundation as you lean into 2021.</p>
+        <img src="https://images03.nicepage.com/c461c07a441a5d220e8feb1a/ce0e4dba656f5375961f7576/s.jpg?version=" />
+        <p>Images from
+            <a href="https://freepik.com/photos/people">Freepik</a>
+        </p>
+        
 </div>
